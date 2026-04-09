28ba9ede812c37abcbe56e143ea09031c394d75c
S.A. Munshi <sadabmunsiusa999@gmail.com>
2026-04-09 15:15:46 +0530

    Revert "Replace ua-parser-js with native user-agent parsing"

--- lib/device-info.ts
+++ lib/device-info.ts
@@ -1,26 +1,20 @@
+import UAParser from 'ua-parser-js'
+
 export function getDeviceInfo(userAgent: string) {
-  const ua = userAgent.toLowerCase()
+  const parser = new UAParser(userAgent)
+  const result = parser.getResult()
 
-  // Detect device type
+  const browser = result.browser.name ?? 'Unknown Browser'
+  const os = result.os.name ?? 'Unknown OS'
+  const deviceType = result.device.type ?? 'desktop'
+
   let device_name = 'Desktop'
-  if (/mobile|android|iphone|ipod/.test(ua)) device_name = 'Mobile'
-  else if (/ipad|tablet/.test(ua)) device_name = 'Tablet'
+  if (deviceType === 'mobile') device_name = 'Mobile'
+  else if (deviceType === 'tablet') device_name = 'Tablet'
 
-  // Detect browser
-  let browser = 'Unknown Browser'
-  if (ua.includes('edg/')) browser = 'Edge'
-  else if (ua.includes('opr/') || ua.includes('opera')) browser = 'Opera'
-  else if (ua.includes('chrome')) browser = 'Chrome'
-  else if (ua.includes('safari')) browser = 'Safari'
-  else if (ua.includes('firefox')) browser = 'Firefox'
-
-  // Detect OS
-  let os = 'Unknown OS'
-  if (ua.includes('windows')) os = 'Windows'
-  else if (ua.includes('android')) os = 'Android'
-  else if (ua.includes('iphone') || ua.includes('ipad')) os = 'iOS'
-  else if (ua.includes('mac')) os = 'macOS'
-  else if (ua.includes('linux')) os = 'Linux'
-
-  return { device_name, browser, os }
+  return {
+    device_name,
+    browser,
+    os,
+  }
 }
