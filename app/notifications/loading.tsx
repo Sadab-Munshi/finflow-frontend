ddcdbda6f4a7bbf9e6a3b6767423b3834594bf90
Sadab Munshi <sadabmunsiusa999@gmail.com>
2026-05-26 11:28:24 +0530

    Add loading component for notifications

---
+++ app/notifications/loading.tsx
@@ -0,0 +1,6 @@
+import Layout from '@/components/layout/Layout'
+import NotificationsSkeleton from '@/components/skeletons/NotificationsSkeleton'
+
+export default function NotificationsLoading() {
+  return <Layout><NotificationsSkeleton /></Layout>
+}
