1434c734dc844669453283fe0d934264bf5a43d1
sa-munshi <sadabmunsiusa999@gmail.com>
2026-05-27 19:46:38 +0530

    Fix settings skeleton loading state

--- app/settings/loading.tsx
+++ app/settings/loading.tsx
@@ -1,5 +1,6 @@
+import Layout from '@/components/layout/Layout'
 import SettingsSkeleton from '@/components/skeletons/SettingsSkeleton'
 
 export default function SettingsLoading() {
-  return <SettingsSkeleton />
+  return <Layout><SettingsSkeleton /></Layout>
 }
