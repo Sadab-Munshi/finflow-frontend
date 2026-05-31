dfb2eea6b14b7a53c49a33b4ab55c2b369e00385
Sadab Munshi <sadabmunsiusa999@gmail.com>
2026-05-31 23:09:46 +0530

    Update sitemap URLs to new domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://app.sadabmunshi.online',
+      url: 'https://www.app.sadabmunshi.me',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
     },
     {
-      url: 'https://app.sadabmunshi.online/login',
+      url: 'https://www.app.sadabmunshi.me/login',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
     },
     {
-      url: 'https://app.sadabmunshi.online/signup',
+      url: 'https://www.app.sadabmunshi.me/signup',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
